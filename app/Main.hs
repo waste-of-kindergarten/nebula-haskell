2a4f9f5811aeb7172164b6496ae1b46ab1342206
waste-of-kindergarten <2020141461068@stu.scu.edu.cn>
2024-02-11 17:04:38 +0800

    iniial commit

---
+++ app/Main.hs
@@ -0,0 +1,7 @@
+module Main where
+
+import Cursor 
+
+main :: IO ()
+main = configJSON >>= print
+
---
+++ lib/Cursor.hs
@@ -0,0 +1,32 @@
+{-# LANGUAGE DeriveGeneric, DeriveAnyClass, DerivingStrategies #-}
+module Cursor where 
+
+import Z.IO
+import System.Process
+import qualified Z.IO.FileSystem as FS
+import qualified Z.Data.JSON as JSON
+import qualified Z.Data.Text as T
+import qualified Z.Data.Builder as Builder
+import qualified Z.Data.CBytes as C
+import GHC.Generics (Generic)
+
+configText :: IO T.Text
+configText = FS.readTextFile $ C.pack "config.json"
+
+data Configuration = Configuration {
+  address :: T.Text,
+  port :: T.Text,
+  user :: T.Text,
+  password :: T.Text
+  } deriving(Show, Generic)
+    deriving anyclass (JSON.JSON)
+
+configJSON :: IO (Maybe Configuration)
+configJSON = do 
+  config <- configText
+  let maybe_config = JSON.decodeText' config :: Either JSON.DecodeError Configuration
+  case maybe_config of
+    (Right x) -> return $ Just x 
+    (Left _) -> return $ Nothing
+
+
